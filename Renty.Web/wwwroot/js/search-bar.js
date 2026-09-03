cf7761f579cd5df77c5eff45d797e4d94b4acd58
Alexey Dyryvolkov <ald@siscon.dk>
2026-09-04 00:18:17 +0200

    forgot js file

---
+++ Renty.Web/wwwroot/js/search-bar.js
@@ -0,0 +1,29 @@
+﻿(
+    function() {
+        var dateSegment = document.getElementById('dateSegment');
+        var datePopover = document.getElementById('datePopover');
+
+        dateSegment.addEventListener('click', function() {
+            datePopover.hidden ? open() : close();
+        })
+
+
+        function open() {
+            datePopover.hidden = false;
+            document.addEventListener('click', onOutsideClick, true);
+            document.addEventListener('keydown', onEscape);
+        }
+        function close() {
+            datePopover.hidden = true;
+            document.removeEventListener('click', onOutsideClick, true);
+            document.removeEventListener('keydown', onEscape);
+        }
+
+        function onOutsideClick(e) {
+            if (!datePopover.contains(e.target) && !dateSegment.contains(e.target)) close();
+        }
+        function onEscape(e) {
+            if (e.key == 'Escape') close();
+        }
+    }
+)();
