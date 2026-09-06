74a217ba08277cd338534061f270b2d1f50595d1
Alexey Dyryvolkov <ald@siscon.dk>
2026-09-06 12:35:57 +0200

    Верхнее меню теперь нормальное

--- Renty.Web/wwwroot/js/property-details/property-details.js
+++ Renty.Web/wwwroot/js/property-details/property-details.js
@@ -1,5 +1,20 @@
 import { createModal } from '../shared/popover.js';
 
+var propertyNav = document.querySelector('.property-nav');
+var navTrigger = document.getElementById('navTrigger');
+
+if (propertyNav && navTrigger) {
+    var navObserver = new IntersectionObserver(function (entries) {
+        var entry = entries[0];
+        if (entry.isIntersecting) {
+            propertyNav.classList.remove('is-visible');
+        } else if (entry.boundingClientRect.top < 0) {
+            propertyNav.classList.add('is-visible');
+        }
+    });
+    navObserver.observe(navTrigger);
+}
+
 var amenitiesModal = createModal('allAmenitiesModal', 'closeAmenitiesModalBtn');
 
 var showAllAmenitiesBtn = document.getElementById('showAllAmenitiesBtn');
