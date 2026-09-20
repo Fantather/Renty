439716b1cefe69bcd12d554e918e5314422ee819
Alexey Dyryvolkov <ald@siscon.dk>
2026-09-20 20:58:26 +0200

    Починил миграции

---
+++ Renty.Infrastructure/Migrations/20260920185615_AddShowExactLocationAndTagIsActive.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Renty.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowExactLocationAndTagIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "PropertyTags",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowExactLocation",
+                table: "Properties",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "PropertyTags");
+
+            migrationBuilder.DropColumn(
+                name: "ShowExactLocation",
+                table: "Properties");
+        }
+    }
+}
